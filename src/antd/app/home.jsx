7d93a916b13f3f60d0775ae268873d67589ec3f1
shi.pengyan <shi.pengyan@zte.com.cn>
2015-08-28 15:53:56 +0800

    antd validation

--- src/antd/app/home.jsx
+++ src/antd/app/home.jsx
@@ -3,16 +3,28 @@
  */
 import Router from 'react-router';
 
-const {RouteHandler}= Router;
+const {RouteHandler, Link}= Router;
 
 export default class Home extends React.Component {
 
   render() {
     console.log('Antd Home render');
     return (
-      <div>
-        <div>Ant Design Home</div>
-        <RouteHandler/>
+      <div className="container">
+        <div className="panel panel-info">
+          <div className="panel-heading">Ant Design Home</div>
+          <div className="panel-body">
+            <ul className="list-group">
+              <li className="list-group-item"><Link to="/validation">Validation</Link></li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="panel">
+          <div className="panel-body">
+            <RouteHandler/>
+          </div>
+        </div>
       </div>
     );
   }
