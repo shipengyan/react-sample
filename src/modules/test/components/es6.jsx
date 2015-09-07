9ae36e257a332f644b05626343dd0875d7d79859
shi.pengyan <shi.pengyan@zte.com.cn>
2015-09-07 13:44:31 +0800

    es6模板字符串

--- src/modules/test/components/es6.jsx
+++ src/modules/test/components/es6.jsx
@@ -8,11 +8,16 @@ export default class ES6 extends React.Component {
   }
 
   render() {
+    var username = 'shipengyan';
+
     return (
       <div>
-        <button className="btn btn-primary" onClick={this._handleArgumentsClick.bind(this,'1')}>不使用 arguments,
-          采用rest语法... 数组获取参数
-        </button>
+        <div className="row"> 测试模板字符串：{`username is ${username}`}        </div>
+        <div className="row">
+          <button className="btn btn-primary" onClick={this._handleArgumentsClick.bind(this,'1')}>
+            不使用 arguments, 采用rest语法... 数组获取参数
+          </button>
+        </div>
       </div>
     );
   }
